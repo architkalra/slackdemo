0d475195a7a0037e3d902e6e91896f0bf44c7478
Archit Kalra <archit.kalra@gmail.com>
2021-02-20 19:46:10 -0800

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -1,24 +1,18 @@
 from pathlib import Path
 import boto3
+for i in range(0,3):
+    print("Launching EC2 instance #",i,"...")
 
-my_file = Path("/home/ec2-user/.aws/credentials")
+    ec2 = boto3.resource('ec2')
+    instance = ec2.create_instances(
+    ImageId='ami-0e9089763828757e1',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',
+    KeyName='archit',
+    SecurityGroupIds= ['sg-2b4f3303', 'sg-0570c047e91ffdb2c'],
+    TagSpecifications = [{'ResourceType': 'instance','Tags':[{'Key':'Name','Value':'Archit-{i}' }]}]
+    )
 
-if my_file.is_file():
-    print ("Credential file exits! ")
-    for i in range(0,3):
-        print("Launching EC2 instance #",i,"...")
+    print ("Created EC2 instance ",instance[0].id)
 
-        ec2 = boto3.resource('ec2')
-        instance = ec2.create_instances(
-        ImageId='ami-0e9089763828757e1',
-        MinCount=1,
-        MaxCount=1,
-        InstanceType='t2.micro',
-        KeyName='archit',
-        SecurityGroupIds= ['sg-2b4f3303', 'sg-0570c047e91ffdb2c'],
-        TagSpecifications = [{'ResourceType': 'instance','Tags':[{'Key':'Name','Value':'Archit-{i}' }]}]
-        )
-
-        print ("Created EC2 instance ",instance[0].id)
-else:
-    print ("file does not exit.")
