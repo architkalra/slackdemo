d43f0cf54d429b7f0e92003f3a9a19e3ebc244f5
Archit Kalra <archit.kalra@gmail.com>
2021-02-25 15:53:43 -0800

    Adding code comments

--- end.py
+++ end.py
@@ -1,11 +1,16 @@
+# Filename:: ec2.py
+# Author:: Archit Kalra
+# Email:: archit.kalra@gmail.com
+
+#importing rquired modules
 import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 
 env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path.name)
+load_dotenv(dotenv_path=env_path.name) #loading the virtual pyenv
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient(token=os.environ['SLACK_TOKEN']) #initiating the slack weblist, SLACK_TOKEN is an env variable on jenkins instance.
 
-client.chat_postMessage(channel='#slack-integration',text="Your Slack CICD Pipeline Job is finished")
+client.chat_postMessage(channel='#slack-integration',text="Your Slack CICD Pipeline Job is finished") #Posting SLACK message
