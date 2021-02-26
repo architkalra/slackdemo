7e861ae3d7f131a4e6bd5c2652d0d9e1343478ab
Archit Kalra <archit.kalra@gmail.com>
2021-02-25 16:37:26 -0800

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -18,7 +18,7 @@ for i in range(0,3):
     InstanceType='t2.micro',
     KeyName='archit',
     SecurityGroupIds= ['sg-2b4f3303', 'sg-0570c047e91ffdb2c'],
-    TagSpecifications = [{'ResourceType': 'instance','Tags':[{'Key':'Name','Value':'Archit-{i}' }]}]
+    TagSpecifications = [{'ResourceType': 'instance','Tags':[{'Key':'Name','Value':'Archit' }]}]
     )
     #Printing Instance ID for the newly created instance in AWS
     print ("Created EC2 instance ",instance[0].id)
